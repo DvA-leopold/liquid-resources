bdf309e07a6b748687d814dcb07f6a2d68a8c5aa
Vladislav <Leopold.dva@gmail.com>
2015-07-08 22:03:26 +0400

    added abstract pump class

---
+++ core/src/com/liquidresources/game/model/game/world/pumps/PumpT.java
@@ -0,0 +1,17 @@
+package com.liquidresources.game.model.game.world.pumps;
+
+public abstract class PumpT {
+    public PumpT() {
+        isActive = false;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public abstract void update();
+
+
+    private boolean isActive;
+}
+
